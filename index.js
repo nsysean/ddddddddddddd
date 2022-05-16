a322c489a784c1118591df75bbab01ec9ee2fe18
nsysean0 <76026698+nsysean@users.noreply.github.com>
2022-05-16 11:58:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+
+const bot = mineflayer.createBot({
+  host: '7PSMP.aternos.me', //EDIT THE IP
+  port: 60468, //EDIT THE PORT
+  username: 'BOT',
+});
+
+bot.on('chat', (username, message) => {
+  console.log(message)
+})
